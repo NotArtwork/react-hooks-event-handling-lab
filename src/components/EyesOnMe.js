8fd7a5c85085c867e9b2317a333909dd6c29924d
NotArtwork <AntArce3@gmail.com>
2022-07-21 13:26:45 -0400

    commit tree 4f4e1a2bbfabca580e19791cac5612d012dced37

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+const handleBlur = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+const handleFocus = () => {
+    console.log('Good!')
+}
+
+
+
+const EyesOnMe = () => {
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
+
+
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type='password' onChange={() => {
+                console.log('Entering password...')
+            }} />
+        </div>
     )
 }
 
